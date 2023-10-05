53019182e4443eab0936eda0df3195ce4d57eb37
Bartosz Nowak <bartekn337@gmail.com>
2023-10-05 21:48:36 +0200

    restructure and cleanup

--- landing/src/routes/layout.tsx
+++ landing/src/routes/layout.tsx
@@ -2,10 +2,7 @@ import { component$, Slot, useStyles$ } from "@builder.io/qwik";
 import { routeLoader$ } from "@builder.io/qwik-city";
 import type { RequestHandler } from "@builder.io/qwik-city";
 
-import Header from "~/components/starter/header/header";
-import Footer from "~/components/starter/footer/footer";
-
-import styles from "./styles.css?inline";
+import componentStyles from "components/lib/components.css?inline";
 
 export const onGet: RequestHandler = async ({ cacheControl }) => {
   // Control caching for this request for best performance and to reduce hosting costs:
@@ -25,14 +22,14 @@ export const useServerTimeLoader = routeLoader$(() => {
 });
 
 export default component$(() => {
-  useStyles$(styles);
+  useStyles$(componentStyles);
   return (
     <>
-      <Header />
+      {/* <Header /> */}
       <main>
         <Slot />
       </main>
-      <Footer />
+      {/* <Footer /> */}
     </>
   );
 });
--- pay/src/routes/layout.tsx
+++ pay/src/routes/layout.tsx
@@ -2,7 +2,7 @@ import { component$, Slot, useStyles$ } from "@builder.io/qwik";
 import { routeLoader$ } from "@builder.io/qwik-city";
 import type { RequestHandler } from "@builder.io/qwik-city";
 
-import styles from "./styles.css?inline";
+import componentStyles from "components/lib/components.css?inline";
 
 export const onGet: RequestHandler = async ({ cacheControl }) => {
   // Control caching for this request for best performance and to reduce hosting costs:
@@ -22,7 +22,7 @@ export const useServerTimeLoader = routeLoader$(() => {
 });
 
 export default component$(() => {
-  useStyles$(styles);
+  useStyles$(componentStyles);
   return (
     <>
       {/* <Header /> */}
